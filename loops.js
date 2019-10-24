2fe1fcc81f86d8be4153d363046120777a6cf8dd
Sundar Maharjan <sundarmaharjan43@gmail.com>
2019-10-24 19:36:33 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+var 
+
+function forloop(array) {
+for (let i = 1; i < 25; i++) {
+  if(i === 1) {
+    array.push("i am 1 strange loop");
+  } else {
+  array.push ("i am $(i} strange loops");
+  }
+ 
+   }return array;
+}
+
+function whileLoop (array) {
+  for (let countdown > 0) {
+    console.log(--countdown);
+  }
+}
+
+
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {
+ 
+} while (incrementVariable() < 5);
+ console.log("i run once regardless.")
+ }
